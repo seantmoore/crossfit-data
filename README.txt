7f1903a83ab0476bc95308842d53c2d16ba2c358
smoore22@gmail.com <smoore22@gmail.com>
2018-03-11 23:11:27 -0400

    Removed generated files. Will backup zipped data on dropbox.

--- README.txt
+++ README.txt
@@ -28,12 +28,35 @@ The `results/csv/` folder contains simplified files for each region. Open them i
 0 - RX
 1 - Scaled
 
+## Division IDs:
+1 - Male
+2 - Female
+3 - Men (45-49)
+4 - Women (45-49)
+5 - Men (50-54)
+6 - Women (50-54)
+7 - Men (55-59)
+8 - Women (55-59)
+9 - Men (60+)
+10 - Women (60+)
+11 - Team
+12 - Men (40-44)
+13 - Women (40-44)
+14 - Boys (14-15)
+15 - Girls (14-15)
+16 - Boys (16-17)
+17 - Girls (16-17)
+18 - Men (35-39)
+19 - Women (35-39)
+
 ## File name examples:
 
-The file `region_11_rx_0_page_21.json` would be data for region 11 (North East), RX athletes, page 21.
+The file `division_1_region_11_rx_0_page_21.json` would be data for males, region 11 (North East), RX athletes, page 21.
 
-The file `region_11_rx_0.csv` would be aggregated data for region 11 (North East), for RX athletes.
+The file `division_2_region_11_rx_0.csv` would be aggregated data for females, region 11 (North East), for RX athletes.
 
 # TO-DO
 
 - Update the scripts to take in parameter names which control region, scaled, etc.
+- Figure out the rate limit. Rough estimate is ~10k requests per hour.
+- Add support for detecting 403 errors (forbidden, which is apparently their code for rate limited).
--- getdata.py
+++ getdata.py
@@ -1,28 +1,48 @@
 #! /usr/bin/python3
 import requests
 
-BASE_URL = 'https://games.crossfit.com/competitions/api/v1/competitions/open/2017/leaderboards?division=1&region=%d&scaled=%d&sort=0&occupation=0&page=%d'
+BASE_URL = 'https://games.crossfit.com/competitions/api/v1/competitions/open/2017/leaderboards?division=%d&region=%d&scaled=%d&sort=0&occupation=0&page=%d'
 
-BASE_FILENAME = 'region_%d_rx_%d_page_%d.json'
+BASE_FILENAME = 'division_%d_region_%d_rx_%d_page_%d.json'
 
-scaled = [1] # [0,1]
-regions = [0] # [r for r in range(17)] # region 0 is worldwide
+divisions = [d for d in range(1,20)] #[1,2] # 1 = male, 2 = female
+scaled = [0,1]
+regions = [r for r in range(1,18)] # region 0 is worldwide
 
-for s in scaled:
-    for r in regions:
-        # get first page
-        first_page_data = requests.get(BASE_URL % (r, s, 1)).json()
-        total_pages = first_page_data['totalpages']
-        for p in range(1,total_pages+1):
-            url = BASE_URL % (r, s, p)
-            filename = BASE_FILENAME % (r, s, p)
-            print('region: %d, scaled?: %d, page: %d / %d' % (r, s, p, total_pages))
-            data = requests.get(url)
-            with open(filename, 'w') as f:
-                f.write(data.text)
-            # end with
-        # end for f
-    # end for r
-# end for s
+for d in divisions:
+    for s in scaled:
+        for r in regions:
+            # get first page
+            first_page_data = requests.get(BASE_URL % (d, r, s, 1)).json()
+            total_pages = first_page_data['totalpages']
+            for p in range(1,total_pages+1):
+                url = BASE_URL % (d, r, s, p)
+                filename = BASE_FILENAME % (d, r, s, p)
+                print('division: %d, region: %d, scaled?: %d, page: %d / %d' % (d, r, s, p, total_pages))
+                data = requests.get(url)
+                _ = data.json() # throws error if HTML
+                with open(filename, 'w') as f:
+                    f.write(data.text)
+                # end with
+            # end for f
+        # end for r
+    # end for s
+# end for d
 
 print("done")
+
+
+
+
+
+'''
+
+
+
+TO-DO: region 0
+
+
+
+
+
+'''
